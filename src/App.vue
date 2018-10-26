3f491ff680ef25021cb680e09f0ac4e47875f5f8
Dave Singh <dave.singh@hbc.com>
2018-10-26 16:24:56 -0400

    Improve rendering on the table.

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,41 @@
       <el-table
     :data="tableData"
     border
+    :default-sort="{prop: 'buildStatus', order: 'ascending'}"
     style="width: 100%">
      <el-table-column
+      sortable
       prop="buildProvider"
       label="Build Provider"
       width="180">
     </el-table-column>
     <el-table-column
+      sortable
       prop="name"
       label="Name"
       width="180">
     </el-table-column>
     <el-table-column
+      sortable
       prop="updated"
       label="Updated"
       width="180">
     </el-table-column>
     <el-table-column
+      sortable
       prop="created"
       label="Created">
     </el-table-column>
     <el-table-column
+      sortable
       prop="buildStatus"
-      label="Build Status">
+      label="Build Status"
+      :filter-method="filterTag">
+      <template slot-scope="scope">
+        <el-tag
+          :type="tagType(scope)"
+          disable-transitions>{{scope.row.buildStatus}}</el-tag>
+      </template>
     </el-table-column>
   </el-table>
     </div>
@@ -42,11 +54,33 @@ export default {
   components: {
     HelloWorld
   },
+  methods: {
+    tagType({row}) {
+      if (row.buildStatus === 'Pending') {
+        return 'warning';
+      } 
+      if (row.buildStatus === 'Fail') {
+        return 'danger';
+      } else if (row.buildStatus === 'Pass') {
+        return 'success';
+      }
+      return '';
+    },
+    filterTag(value, row) {
+    return row.buildStatus === value;
+    },
+  },
   data() {
         return {
           tableData: [{
             buildProvider: 'AWS',
             name: 'Wham',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Fail'
+          }, {
+            buildProvider: 'AWS',
+            name: 'CSI',
             updated: '2016-05-03',
             created: '2016-05-03',
             buildStatus: 'Pass'
@@ -58,16 +92,76 @@ export default {
             buildStatus: 'Pass'
           }, {
             buildProvider: 'AWS',
-            name: 'Avengers',
+            name: 'CSI',
             updated: '2016-05-03',
             created: '2016-05-03',
             buildStatus: 'Pass'
           }, {
             buildProvider: 'AWS',
-            name: 'Vision',
+            name: 'CSI',
             updated: '2016-05-03',
             created: '2016-05-03',
             buildStatus: 'Pass'
+          }, {
+            buildProvider: 'AWS',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pass'
+          }, {
+            buildProvider: 'AWS',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pass'
+          }, {
+            buildProvider: 'AWS',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pass'
+          }, {
+            buildProvider: 'AWS',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pass'
+          }, {
+            buildProvider: 'Travis',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pass'
+          }, {
+            buildProvider: 'Travis',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pass'
+          }, {
+            buildProvider: 'AWS',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Fail'
+          }, {
+            buildProvider: 'AWS',
+            name: 'CSI',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pending'
+          }, {
+            buildProvider: 'AWS',
+            name: 'Avengers',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Pending'
+          }, {
+            buildProvider: 'Travis',
+            name: 'Vision',
+            updated: '2016-05-03',
+            created: '2016-05-03',
+            buildStatus: 'Fail'
           }]
         }
       }
