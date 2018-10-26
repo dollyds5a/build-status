c0b0929ca192c1d9b62f77d3477459fc8d1c4829
Barry O'Neill <barra.oneill@gmail.com>
2018-10-26 16:22:59 -0400

    first pass at aws pipeline list

--- src/api/api.js
+++ src/api/api.js
@@ -1,25 +1,36 @@
+const AWS = require('aws-sdk');
+const util = require('util');
+
+const pipelineConfig = {
+
+    aws: {
+        region: 'us-east-1',
+        accessKey: process.env.FART_KEY,
+        secretKey: process.env.FART_SECRET
+    }
+};
 
 exports.listPipelines = (config) => {
 
-    return Promise.resolve(
-        [
-            {
-                id: 'pipeline1',
-                provider: 'aws-codepipeline',
-                url: 'https://console.aws.amazon.com/codepipeline/home?region=us-east-1#/view/pipeline1'
-            },
-            {
-                id: 'pipeline2',
-                provider: 'aws-codedeploy',
-                url: 'https://console.aws.amazon.com/codepipeline/home?region=us-east-1#/view/pipeline1'
-            },
-            {
-                id: 'pipeline3',
-                provider: 'travis-pipeline',
-                url: 'https://console.aws.amazon.com/codepipeline/home?region=us-east-1#/view/pipeline1'
-            }
-        ]
-    );
+    return new AWS.CodePipeline({
+        region: config.aws.region,
+        httpOptions: {timeout: 3000},
+        credentials: { "accessKeyId": config.aws.accessKey, "secretAccessKey": config.aws.secretKey }
+    }).listPipelines()
+    .promise()
+    .then(data => {
+        return data.pipelines.map(p => ({
+            id: p.name,
+            provider: 'aws-codepipeline',
+            url: util.format('https://console.aws.amazon.com/codepipeline/home?region=%s#/view/%s', config.aws.region, p.name)
+        }))
+
+    })
+    .catch(err => {
+        console.log('Could not fetch pipeline list:' + err)
+        return [];
+    });
+
 };
 
 exports.queryPipeline = (config, pipelineId) => {
@@ -34,5 +45,7 @@ exports.queryPipeline = (config, pipelineId) => {
         }
     );
 };
+
+
 
 
